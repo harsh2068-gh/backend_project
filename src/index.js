0f3e523f7c87653a65d972cfabf3e96263aea9d3
harsh2608-gh <helloooworld26@gmail.com>
2024-12-01 22:37:50 +0530

    connected to mongo atlas database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("Hello")
+// require("dotenv").config({path:"./env"})
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants";
+import express from "express"
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////normal approach
+// const app =express()
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror",(e)=>{
+//             console.log("Error: ",e)
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (e) {
+//         console.error("Error: ", e)
+//         throw err
+//     }
+// })()
